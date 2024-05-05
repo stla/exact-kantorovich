9be7e0595bf8e994a5232bb069eecab0e2cdbbf0
stla <laurent_step@yahoo.fr>
2024-05-05 19:56:23 +0200

    constraints

--- src/Math/Optimization/Kantorovich.hs
+++ src/Math/Optimization/Kantorovich.hs
@@ -5,6 +5,7 @@ import           Data.Map.Strict                 (
                                                    Map
                                                  , fromList
                                                  , mapKeys
+                                                 , singleton
                                                  )
 import qualified Data.Map.Strict                 as DM
 import           Linear.Simplex.Solver.TwoPhase  (
@@ -24,5 +25,33 @@ import           Linear.Simplex.Types            (
                                                  , Dict ( .. )
                                                  )
 
+type IntIntMap = Map (Int, Int) Rational
+
 stack :: Int -> (Int, Int) -> Int
 stack ncol (i, j) = (i - 1) * ncol + j 
+
+constraints :: [Rational] -> [Rational] -> [PolyConstraint]
+constraints mu nu = 
+  positivityConstraints ++ rowMarginsConstraints ++ colMarginsConstraints
+  where
+    m = length mu
+    n = length nu
+    rows = [ 1 .. m ]
+    cols = [ 1 .. n ]
+    positivityConstraints = 
+      [ GEQ { lhs = singleton (stack n (i, j)) 1, rhs = 0 } 
+            | i <- rows, j <- cols ]
+    rowMarginsConstraints = concat
+      [ EQ { 
+             lhs = fromList [ (stack n (i, j), 1) | j <- cols ]
+           , rhs = mu !! (i-1) 
+           } 
+        | i <- rows ]
+    colMarginsConstraints = concat
+      [ EQ { 
+             lhs = fromList [ (stack n (i, j), 1) | i <- rows ]
+           , rhs = nu !! (j-1) 
+           } 
+        | j <- cols ]
+
+

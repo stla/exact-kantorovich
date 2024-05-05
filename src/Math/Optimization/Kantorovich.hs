6a495771cd3d4ba4b629a0dade39381dfa620140
stla <laurent_step@yahoo.fr>
2024-05-05 23:17:54 +0200

    kantorovich

--- src/Math/Optimization/Kantorovich.hs
+++ src/Math/Optimization/Kantorovich.hs
@@ -1,8 +1,15 @@
 module Math.Optimization.Kantorovich
   ( 
     kantorovich
+  , test
+  , getObjectiveValueAndSolution
   ) where
 import           Prelude                hiding   ( EQ )
+import           Control.Monad.Logger            (
+                                                   MonadLoggerIO
+                                                 , LoggingT
+                                                 , runStdoutLoggingT
+                                                 )
 import           Data.Map.Strict                 ( 
                                                    Map
                                                  , fromList
@@ -10,6 +17,16 @@ import           Data.Map.Strict                 (
                                                  , singleton
                                                  )
 import qualified Data.Map.Strict                 as DM
+import           Data.Maybe                      (
+                                                   isJust
+                                                 , fromJust
+                                                 )
+import           Data.Ratio                      (
+                                                   (%)
+                                                 )
+import           Data.Tuple.Extra                (
+                                                   both
+                                                 )
 import           Linear.Simplex.Solver.TwoPhase  (
                                                    twoPhaseSimplex
                                                  )
@@ -23,7 +40,7 @@ import           Linear.Simplex.Types            (
                                                  , PolyConstraint ( .. )
                                                  , ObjectiveFunction ( .. )
                                                  , DictValue ( .. )
-                                                 , Dict ( .. )
+                                                 , Dict
                                                  )
 import           Linear.Simplex.Util             (
                                                    extractObjectiveValue
@@ -37,34 +54,44 @@ stack ncol (i, j) = (i - 1) * ncol + j
 unstack :: Int -> Int -> (Int, Int)
 unstack ncol k = (q + 1, r + 1)
   where
-    (q, r) = quotRem k ncol
+    (q, r) = quotRem (k-1) ncol
 
-getObjectiveValueAndSolution :: Maybe Result -> Int -> (Maybe Rational, Maybe (Map (Int, Int) Rational))
-getObjectiveValueAndSolution maybeResult ncol = 
-  (
-    extractObjectiveValue maybeResult
-  , fmap f maybeResult
-  )
+getObjectiveValueAndSolution :: Int -> Maybe Result -> Maybe (Rational, Map (Int, Int) Rational)
+getObjectiveValueAndSolution ncol maybeResult = 
+  if isJust maybeResult
+    then 
+      Just 
+        (
+          fromJust $ extractObjectiveValue maybeResult
+        , fromJust $ fmap f maybeResult
+        )
+    else
+      Nothing
   where
     f :: Result -> Map (Int, Int) Rational
     f (Result var varLitMap) = mapKeys (unstack ncol) (DM.delete var varLitMap)
 
 kantorovich :: 
-  [Rational] -> [Rational] -> ((Int, Int) -> Rational) -> IO (Maybe Result)
-kantorovich mu nu dist = twoPhaseSimplex objFunc polyConstraints
+  [(a, Rational)] -> [(a, Rational)] -> ((a, a) -> Rational) -> IO (Maybe (Rational, Map (Int, Int) Rational))
+kantorovich wxs wys dist = do 
+  maybeResult <- runStdoutLoggingT $ twoPhaseSimplex objFunc polyConstraints
+  return $ getObjectiveValueAndSolution ncol maybeResult
   where
-    m = length mu
-    n = length nu
-    objFunc = objectiveFunction m n dist
+    ncol = length wys
+    (xs, mu) = unzip wxs
+    (ys, nu) = unzip wys
+    objFunc = objectiveFunction xs ys dist
     polyConstraints = constraints mu nu 
 
 objectiveFunction :: 
-  Int -> Int -> ((Int, Int) -> Rational) -> ObjectiveFunction
-objectiveFunction m n dist = Min 
+  [a] -> [a] -> ((a, a) -> Rational) -> ObjectiveFunction
+objectiveFunction xs ys dist = Min 
   { 
-    objective = fromList [ (stack n (i, j), dist (i, j)) | i <- rows, j <- cols ]
+    objective = fromList [ (stack n (i, j), dist (xs !! (i-1), ys !! (j-1))) | i <- rows, j <- cols ]
   }
   where
+    m = length xs
+    n = length ys
     rows = [ 1 .. m ]
     cols = [ 1 .. n ]
 
@@ -92,4 +119,11 @@ constraints mu nu =
            } 
         | j <- cols ]
 
+dist01 :: (Int, Int) -> Rational
+dist01 (i, j) = toRational (fromEnum (i /= j))
 
+mu, nu :: [Rational] 
+mu = [1%7, 2%7, 4%7]
+nu = [1%4, 1%4, 1%2]
+
+test = kantorovich (zip [0 ..] mu) (zip [0 ..] nu) dist01
